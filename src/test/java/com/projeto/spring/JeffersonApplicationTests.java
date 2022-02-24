d8ce4b574711e8c175bc3eddd69cf0a23892c2a2
Jefferson Felipe <jeff.keiryw@gmail.com>
2022-02-24 17:51:23 -0300

    Projeto inicial criado

---
+++ src/test/java/com/projeto/spring/JeffersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeffersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
